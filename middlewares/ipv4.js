4b4dac32f94f97996464b00298073483ae91e89b
KimJongkyu <hisprit@naver.com>
2025-12-12 13:21:23 +0900

    again

--- middlewares/ipv4.js
+++ middlewares/ipv4.js
@@ -1,14 +1,13 @@
 function getIpv4(ip) {
-  // ::ffff:127.0.0.1
-  // :로 나눠서 배열을 만들면 마지막 배열에 ip가 있다. 
-  const arr = ip.split(":");
-  return arr[arr.length - 1];
+	//::ffff:127.0.0.1
+	// : 로 나눠서 배열로 만들면 마지막 배열에 ip있다.
+	const arr = ip.split(":");
+	return arr[arr.length-1];
 }
 
 module.exports = async (ctx, next) => {
-  // ctx.request.headers['x-forwarded-for'] 는 Nginx에서 헤더에 넣은 실제 IP
-  console.log("con ip --> ", ctx.request.headers['x-forwarded-for'])
-  ctx.ipv4 = ctx.request.headers['x-forwarded-for'] || getIpv4(ctx.ip);
-  await next();
-
+	// headers['x-forwarded-for'] 는 NginX에서 해더에 넣은 실제 접속 IP
+	// console.log('conn ip -->',ctx.request.headers['x-forwarded-for'])
+	ctx.ipv4 = ctx.request.headers['x-forwarded-for'] ||  getIpv4(ctx.ip);
+	await next();
 }
